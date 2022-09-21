a732a9bcde0c8ed05983aabafc489a39a8c09d00
BT_4N2_18 <jxu5406@bths.edu>
2022-09-21 11:30:18 -0400

    Patchnotes v0.01
    Project created
    
    Added Scanner my beloved
    
    Declared:
    myBeloved
    bill
    tipPercent
    headCount

---
+++ src/Project1.java
@@ -0,0 +1,12 @@
+import  java.util.Scanner;
+public class Project1 {
+    public static void main(String[] args) {
+        Scanner myBeloved = new Scanner(System.in); //Scanner my beloved
+        
+        double bill = myBeloved.nextDouble();
+
+        int tipPercent = myBeloved.nextInt();
+
+        int headCount = myBeloved.nextInt();
+    }
+}
