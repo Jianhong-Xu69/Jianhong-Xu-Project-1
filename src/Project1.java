171376f5bd348a79cdb84bdf4578ed9f8d9253ee
jianx <jxu5406@bths.edu>
2022-09-21 17:51:14 -0400

    Patchnotes v1.00
    Added
    totalBill
    totalEach
    totalBillString
    totalEachString
    
    Added operations to
    totalBill
    totalEach
    
    Following variables now have string conversions
    totalBill
    totalEach
    
    Reworked decimal formatting
    
    Added comments

--- src/Project1.java
+++ src/Project1.java
@@ -1,39 +1,42 @@
-import java.util.Scanner;
+import java.text.DecimalFormat;//formatting decimals
+import java.text.NumberFormat;//formatting
+import java.util.Scanner;//inputs
 public class Project1 {
     public static void main(String[] args) {
         Scanner myBeloved = new Scanner(System.in); //Scanner my beloved
+        NumberFormat formatter = new DecimalFormat("#0.00");//shamelessly stolen off of a web forum I sure hope this works
 
-        System.out.println("How much was your bill?");
+        System.out.println("How much was your bill?");//ask for the amount billed
         double bill = myBeloved.nextDouble();
 
-        System.out.println("What percentage are you tipping? (as an integer value)");
+        System.out.println("What percentage are you tipping? (as an integer value)");//ask for the percent tip
         int tipPercent = myBeloved.nextInt();
         double actualPercent = (double) tipPercent / 100;
 
-        System.out.println("How many people are in your party?");
+        System.out.println("How many people are in your party?");//ask for amount of people
         int headCount = myBeloved.nextInt();
 
-        double tipTotal = bill * actualPercent;
+        double tipTotal = bill * actualPercent;//calculations
         double tipEach = tipTotal / headCount;
+        double totalBill = bill + tipTotal;
+        double totalEach = totalBill / headCount;
 
-        tipTotal = Math.round(tipTotal * 100.00) / 100.00;
+        tipTotal = Math.round(tipTotal * 100.00) / 100.00;//rounding
         tipEach = Math.round(tipEach * 100.00) / 100.00;
 
-        String billString = String.valueOf(bill);
-        String tipPercentString = String.valueOf(tipPercent);
+        String billString = (formatter.format(bill)); //oh my goodness gracious it works
+        String tipPercentString = String.valueOf(tipPercent);//formatting the decimals and type casting to string
         String headCountString = String.valueOf(headCount);
-        String tipTotalString = String.valueOf(tipTotal);
-        String tipEachString = String.valueOf(tipEach);
+        String tipTotalString = (formatter.format(tipTotal));
+        String tipEachString = (formatter.format(tipEach));
+        String totalBillString = (formatter.format(totalBill));
+        String totalEachString = (formatter.format(totalEach));
 
-        System.out.println();
+        System.out.println();//printing out the results
         System.out.println("Your bill is $" + billString);
         System.out.println("Your tip percentage is " + tipPercentString + "%");
         System.out.println("You have " + headCountString + " people in your party" );
-        System.out.println("Your total tip is $" + tipTotalString);
-        System.out.println("Your individual tip is $" + tipEachString + " each");
+        System.out.println("Your total bill is $" + totalBillString + ", your individual bill is $" + totalEachString + " each");
+        System.out.println("Your total tip is $" + tipTotalString + ", your individual tip is $" + tipEachString + " each");
     }
 }
-//small note to myself after I closed by file:
-//NumberFormat formatter = new DecimalFormat("#0.00");     
-//System.out.println(formatter.format(4.0));
-//Attempt this later at home
