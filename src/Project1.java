2cfcd46fff1f1cb07267a72eeade9859f11ced87
BT_4N2_18 <jxu5406@bths.edu>
2022-09-21 11:56:49 -0400

    Patchnotes v0.02
    Declared:
    actualPercent
    tipTotal
    tipEach
    billString
    tipPercentString
    headCountString
    tipTotalString
    tipEachString
    
    Added operations to:
    actualPercent
    tipTotal
    tipEach
    
    Added print instructions for:
    bill
    tipPercent
    headCount
    
    Following variables now have string conversions
    bill
    tipPercent
    headCount
    tipTotal
    tipEach
    
    Added prints for
    billString
    tipPercentString
    headCountString
    tipTotalString
    tipEachString

--- src/Project1.java
+++ src/Project1.java
@@ -2,11 +2,30 @@ import  java.util.Scanner;
 public class Project1 {
     public static void main(String[] args) {
         Scanner myBeloved = new Scanner(System.in); //Scanner my beloved
-        
+
+        System.out.println("How much was your bill?");
         double bill = myBeloved.nextDouble();
 
+        System.out.println("What percentage are you tipping? (as an integer value)");
         int tipPercent = myBeloved.nextInt();
+        double actualPercent = (double) tipPercent / 100;
 
+        System.out.println("How many people are in your party?");
         int headCount = myBeloved.nextInt();
+
+        double tipTotal = bill * actualPercent;
+        double tipEach = tipTotal / headCount;
+
+        String billString = String.valueOf(bill);
+        String tipPercentString = String.valueOf(tipPercent);
+        String headCountString = String.valueOf(headCount);
+        String tipTotalString = String.valueOf(tipTotal);
+        String tipEachString = String.valueOf(tipEach);
+
+        System.out.println("Your bill is $" + billString);
+        System.out.println("Your tip percentage is " + tipPercentString + "%");
+        System.out.println("You have " + headCountString + " people in your party" );
+        System.out.println("Your total tip is $" + tipTotalString);
+        System.out.println("Your individual tip is $" + tipEachString + " each");
     }
 }
