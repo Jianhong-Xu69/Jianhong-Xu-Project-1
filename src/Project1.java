f99186b25ae2fe98496aea070c665488dcd92d8e
BT_4N2_18 <jxu5406@bths.edu>
2022-09-21 12:06:08 -0400

    Patchnotes v0.03
    Revised formatting
    
    *EXPERIMENTAL* added rounding

--- src/Project1.java
+++ src/Project1.java
@@ -1,4 +1,4 @@
-import  java.util.Scanner;
+import java.util.Scanner;
 public class Project1 {
     public static void main(String[] args) {
         Scanner myBeloved = new Scanner(System.in); //Scanner my beloved
@@ -16,12 +16,16 @@ public class Project1 {
         double tipTotal = bill * actualPercent;
         double tipEach = tipTotal / headCount;
 
+        tipTotal = Math.round(tipTotal * 100.00) / 100.00;
+        tipEach = Math.round(tipEach * 100.00) / 100.00;
+
         String billString = String.valueOf(bill);
         String tipPercentString = String.valueOf(tipPercent);
         String headCountString = String.valueOf(headCount);
         String tipTotalString = String.valueOf(tipTotal);
         String tipEachString = String.valueOf(tipEach);
 
+        System.out.println();
         System.out.println("Your bill is $" + billString);
         System.out.println("Your tip percentage is " + tipPercentString + "%");
         System.out.println("You have " + headCountString + " people in your party" );
